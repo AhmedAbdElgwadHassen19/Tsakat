8e98bf9b1c0ef3facd42b264996feace609a85da
Ahmed AbdElgwad <Ahmed AbdElgwad@example.com>
2025-04-23 18:24:01 +0200

    Fix: corrected Signup import path for Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./components/Signup";
+import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import AdminPage from "./Components/Admin";
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/goals" element={<Goals/>} />
         <Route path="/admin" element={<AdminPage />} />
